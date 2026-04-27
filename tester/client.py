3d59be9de546136edbafe0235cb0f340de26400c
sonnya3 <sonnyamapemo37@gmail.com>
2026-04-27 18:55:59 +0200

    Update client.py

--- tester/client.py
+++ tester/client.py
@@ -1,1 +1,12 @@
+import requests
+import time
 
+def call_agify(name="michael"):
+    url = f"https://api.agify.io?name={name}"
+    start_time = time.time()
+    try:
+        response = requests.get(url, timeout=10)
+        latency = round(time.time() - start_time, 3)
+        return response, latency
+    except Exception as e:
+        return None, 0
