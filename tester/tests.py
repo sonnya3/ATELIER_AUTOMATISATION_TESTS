0d1d7e651fc6d02e291b7957b1da1d1a029dcd9f
sonnya3 <sonnyamapemo37@gmail.com>
2026-04-27 18:56:39 +0200

    Update tests.py

--- tester/tests.py
+++ tester/tests.py
@@ -1,1 +1,7 @@
+def validate_contract(data):
+    """Vérifie si les clés JSON attendues sont présentes"""
+    required_keys = ["name", "age", "count"]
+    return all(key in data for key in required_keys)
 
+def check_status_code(response):
+    return response.status_code == 200
